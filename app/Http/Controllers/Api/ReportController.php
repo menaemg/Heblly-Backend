4e2d388db640507b934e0af5ee621589a2a680eb
Abdalmonaem <abdalmonaem.gamal@gmail.com>
2023-06-27 14:41:18 +0300

    update report request

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -7,21 +7,18 @@ use App\Models\Post;
 use App\Models\Gratitude;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\api\report\ReportRequest;
 use Illuminate\Support\Facades\Auth;
 
 class ReportController extends Controller
 {
-    public function report(Post $post, Request $request)
+    public function report(Post $post, ReportRequest $request)
     {
         // if($post->user_id == Auth::id()) {
-        //     return \jsonResponse(false, 'you can\'t report you Pick', null, 401);
+        //     return \jsonResponse(false, 'you can\'t report your post', null, 401);
         // }
 
-        $post->report()->create([
-            'reason' => 'reason',
-            'description' => 'desc',
-            'user_id' => Auth::id()
-        ]);
+        $post->report()->create($request->validated()+ ['user_id' => Auth::id()]);
 
         return jsonResponse(true, 'report sent successfully');
     }
---
+++ app/Http/Requests/Api/report/ReportRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\api\report;
+
+use App\Http\Requests\BaseFormRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReportRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'reason' => 'required|string|max:255',
+            'description' => 'required|string|max:9999',
+        ];
+    }
+}
