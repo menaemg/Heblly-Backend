333062162497a08dbef8b39b50ee958733c3aa7e
Abdalmonaem <abdalmonaem.gamal@gmail.com>
2023-07-16 21:36:16 +0300

    update PostWishController

--- app/Http/Resources/PostWishResource.php
+++ app/Http/Resources/PostWishResource.php
@@ -14,31 +14,33 @@ class PostWishResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'body' => $this->body,
-            'location' => $this->location,
-            'main_image' => $this->main_image,
-            'user' => [
-                'id' => $this->user->id,
-                'username' => $this->user->username,
-                'name' => $this->user->profile ? $this->user->profile->full_name : $this->user->username,
-                'avatar' => $this->user->profile ? $this->user->profile->avatar_url : null,
-                'cover' => $this->user->profile ? $this->user->profile->cover_url : null,
-                'followers_count' => $this->user->followers()->count(),
-                'is_private' => $this->user->profile && $this->user->profile->privacy == 'private' ? true :false,
-                'bio' => $this->user->profile ? $this->user->profile->bio : null,
-                'location' => $this->user->profile ? $this->user->profile->location : null,
-            ],
-            'tags' => $this->tags ? $this->tags->map(function ($tag) {
-                return $tag->name;
-            }) : [],
-            'more_images' => $this->images,
-            'privacy' => $this->privacy,
-            'access_list' => $this->access_list,
-            'created_at'  => $this->created_at,
-            'updated_at'  => $this->updated_at
-        ];
+        if ($this !== null) {
+            return [
+                'id' => $this->id,
+                'title' => $this->title,
+                'body' => $this->body,
+                'location' => $this->location,
+                'main_image' => $this->main_image,
+                'user' => [
+                    'id' => $this->user->id,
+                    'username' => $this->user->username,
+                    'name' => $this->user->profile ? $this->user->profile->full_name : $this->user->username,
+                    'avatar' => $this->user->profile ? $this->user->profile->avatar_url : null,
+                    'cover' => $this->user->profile ? $this->user->profile->cover_url : null,
+                    'followers_count' => $this->user->followers()->count(),
+                    'is_private' => $this->user->profile && $this->user->profile->privacy == 'private' ? true :false,
+                    'bio' => $this->user->profile ? $this->user->profile->bio : null,
+                    'location' => $this->user->profile ? $this->user->profile->location : null,
+                ],
+                'tags' => $this->tags ? $this->tags->map(function ($tag) {
+                    return $tag->name;
+                }) : [],
+                'more_images' => $this->images,
+                'privacy' => $this->privacy,
+                'access_list' => $this->access_list,
+                'created_at'  => $this->created_at,
+                'updated_at'  => $this->updated_at
+            ];
+        }
     }
 }
